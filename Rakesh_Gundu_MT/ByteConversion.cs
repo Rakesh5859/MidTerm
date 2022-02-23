14435dc3a9216ab0f51701a0dfe8933b375cc737
Rakesh5859 <rgundu0928@conestogac.on.ca>
2022-02-23 12:34:24 -0500

    prework application

---
+++ Rakesh_Gundu_MT/ByteConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakesh_Gundu_MT
+{
+        public static class ByteConversion
+        {
+            public static double MegabytesToBytes(double Input)
+            {
+                double result = Input * 1024*1024;
+                return result;
+            }
+            public static double GigabytesToMegabytes(double Input)
+            {
+                double result = Input * 1024;
+                return result;
+            }
+            public static double TerabytesToGigabytes(double Input)
+            {
+                double result = Input * 1024;
+                return result;
+            }
+
+        }
+}
